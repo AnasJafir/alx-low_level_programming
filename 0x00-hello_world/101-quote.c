1b7e9433b857d55c251dce0c9d58f300cf62a99b
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-16 04:46:15 -0700

    Print to the standard error with fprintf function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,11 @@
  *
  * Description: Print to the standard error with fpritnf function
  *
- * Return: Always 1 (Success)
+ * Return: 1 (Fail)
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - "\
+"Dora Korpar, 2015-10-19\n");
 return (1);
 }
