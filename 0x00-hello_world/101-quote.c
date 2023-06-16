279b8361ba9af2eed24cd489d0d5048486befbfa
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-16 05:06:40 -0700

    Print to the standard error with fprintf function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Description: Print to the standard error with fpritnf function
+ * Description: Print to the standard error with write function
  *
  * Return: 1 (Fail)
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, "
-"2015-10-19\n");
+const char *A = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(1, A, 59);
 return (1);
 }
