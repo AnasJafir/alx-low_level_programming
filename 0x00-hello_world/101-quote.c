8cc3d4035cae3c744b0dfe329163a62b1958d13e
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-16 05:30:14 -0700

    Print to the standard error with fprintf function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-char *A = "and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19";
+char *A = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 fprintf(stderr, "%s\n", A);
 return (1);
 }
