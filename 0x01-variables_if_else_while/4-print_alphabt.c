81b5a77842751d65c28cf141acd814d31f423319
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-18 09:34:10 -0700

    Print alphabet except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,11 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if (ch == 'e' || ch == 'q')
-			ch++;
+		if (ch != 'e' && ch != 'q')
+		{
 			putchar(ch);
-			ch++;
+		}
+		ch++;
 	}
 	putchar('\n');
 	return (0);
