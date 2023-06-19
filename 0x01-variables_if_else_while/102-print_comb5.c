5eba6069ffd790567757448c1e180e4a5cb29dcf
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-19 07:44:01 -0700

    Print combinations of two two digits numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print combinations of two two-digits numbers
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int num1 = 0, num2, num3, num4;
+
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			num3 = 0;
+			while (num3 <= 9)
+			{
+				num4 = 0;
+				while (num4 <= 9)
+				{
+					if ((num1 + num2) < (num3 + num4))
+					{
+						putchar(num1 + 48);
+						putchar(num2 + 48);
+						putchar(' ');
+						putchar(num3 + 48);
+						putchar(num4 + 48);
+					if ((num1 + num2 + num3 + num4) != 35)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+					num4++;
+				}
+				num3++;
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
+
