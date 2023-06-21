389a9ba57f942e7e09ec500d3e993a75ff609397
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-21 11:08:23 -0700

    print times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,26 +11,30 @@ void times_table(void)
 	{
 		for (z = 0; z <= 9; z++)
 		{
-			if ((y * z) <= 9)
+			if (z != 9)
+			{
+			if ((y * z) == 0)
 			{
 				_putchar((y * z) + 48);
 				_putchar(',');
+			}
+			else if ((y * z) <= 9)
+			{
 				_putchar(' ');
 				_putchar(' ');
+				_putchar((y * z) + 48);
+				_putchar(',');
 			}
 			if ((y * z) > 9)
 			{
+				_putchar(' ');
 				_putchar(((y * z) / 10) + 48);
 				_putchar(((y * z) % 10) + 48);
 				_putchar(',');
-				_putchar(' ');
 			}
-			if (z == 9)
-			{
-				_putchar(((y * z) / 10) + 48);
-				_putchar(((y * z) % 10) + 48);
 			}
 		}
-		_putchar('\n');
 	}
+		_putchar('\n');
 }
+
