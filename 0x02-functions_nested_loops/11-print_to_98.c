63f921f699a46ce9fe9d77e6b9addb631d2f7f88
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-21 14:12:20 -0700

    print all number from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_to_98- print all natural number from 'n' to 98
+ *
+ * @n: integer type input
+*/
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		_putchar((n / 100) + 48);
+		_putchar((n % 100) + 48);
+		_putchar((n % 10) + 48);
+		_putchar(',');
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	if (n != 98)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar((98 / 10) + 48);
+	_putchar((98 % 10) + 48);
+	_putchar('\n');
+}
