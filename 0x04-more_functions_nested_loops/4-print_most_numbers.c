8a4f58a6d4d02116ce3b1a2a87a613d1da750e0d
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-23 08:26:45 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,10 +6,13 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	while (i <= 9 && (i != 2 || i != 4))
+	while (i <= 9)
 	{
-	_putchar(i + 48);
-	i++;
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + 48);
+		}
+		i++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
