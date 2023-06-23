64f0584e316b02272e14ffb61b7d6d883f9cd911
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-23 16:53:32 -0700

    print numbers from 0 to 14 for 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,20 @@
 */
 void more_numbers(void)
 {
-int i, j;
+	int i, j, k;
 
-for (i = 1; i <= 10; i++)
-{
-for (j = 0; j <= 14; j++)
-{
-if (j >= 10)
-{
-_putchar('1');
-_putchar((j % 10) + 48);
-}
-}
-_putchar('\n');
-}
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			k = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				k = j % 10;
+			}
+			_putchar(k + 48);
+		}
+		_putchar('\n');
+	}
 }
