f6de18cbb3bdc482284c267be5334d574c19ada7
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-23 19:50:22 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,7 +6,7 @@
  *        numbers that are multiples of 3 print Fizz
  *        numbers that are multiples of 5 print Buzz
  *        numbers that are multiples of 3 and 5 print FizzBuzz
- *        each number and word should be separated by a space
+ *        each number and word to be separated by a space
  *
  * Return: Always 0 (Success)
 */
@@ -16,15 +16,15 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 && !(i % 5 == 0))
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 5 == 0 && !(i % 3 == 0))
 		{
 			printf("Buzz");
 		}
-		else if (i % 5 == 0 && i % 3 == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
