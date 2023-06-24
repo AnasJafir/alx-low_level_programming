71958470e94ad5fdf876ce92a8331e7c4fae5a9a
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-23 17:12:58 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,17 +10,14 @@ void print_line(int n)
 {
 	int i;
 
-	while (i = n)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		if (n > 0)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar(95);
-		}
-		else if (n <= 0)
-		{
 			_putchar('\n');
 		}
-		i++;
-		_putchar('\n');
 	}
 }
