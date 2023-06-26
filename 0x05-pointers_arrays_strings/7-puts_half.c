1662d47d36ebbb4b38f6fe6e10e8657d137c2987
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-26 16:08:57 -0700

    print half a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,17 +6,15 @@
 */
 void puts_half(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != 0)
-	{
-		_putchar(str[i]);
-		i++;
-	}
+	for (i = 0; str[i] != 0; i++)
+		;
+
+	i++;
 	for (i /= 2; str[i] != 0; i++)
 	{
 		_putchar(str[i]);
-		i++;
 	}
 	_putchar('\n');
 }
