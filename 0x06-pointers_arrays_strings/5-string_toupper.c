7cdbae81e3ab1101d836fb9b924c002f30ceb1e1
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-30 18:25:42 -0700

    change all lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,12 +10,11 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 	
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
-			i++;
 		}
 	}
 	return (str);
