156f83603dd035d9d5d1d6f210518a7cf6d0f346
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-30 17:34:15 -0700

    copy two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,10 +16,10 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 		i++;
-		if (i < n)
-		{
-			dest[i] = '\0';
-		}
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
