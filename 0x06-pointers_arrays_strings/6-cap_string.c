252ae57d1ea1e8cad80f690b8604f9584d09a3f4
AnasJafir <dr.anas.jafir@gmail.com>
2023-06-30 18:48:44 -0700

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,6 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 97 && str[i] <= 122))
-		{
 			i++;
 			if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
 					str[i - 1] == '\n' || str[i - 1] == ',' ||
@@ -22,11 +21,9 @@ char *cap_string(char *str)
 					str[i - 1] == '"' || str[i - 1] == '(' ||
 					str[i - 1] == ')' || str[i - 1] == '{' ||
 					str[i - 1] == '}' || i == 0)
-			{
 				str[i] = str[i] - 32;
-				i++;
-			}
-		}
+
+			i++;
 	}
 	return (str);
 }
