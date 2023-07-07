152f5c9a14698ea5eab2c519a81dc5bf38d8d381
AnasJafir <dr.anas.jafir@gmail.com>
2023-07-07 14:39:13 -0700

    function that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: integer input
+ * @argv: string input
+ * Return: always 0 (Success)
+*/
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	
+	printf("%s\n", argv[0]);
+	return (0);
+}
