5048d9df2843a4f8f6a10d4d9df1846fce370339
AnasJafir <dr.anas.jafir@gmail.com>
2023-07-19 08:02:25 -0700

    feat: print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,5 @@
 #include "function_pointers.h"
 /**
- * f - function prints a given name
- * @name: string input
- * Return: void
-*/
-void f(char *name)
-{
-	printf("Hello, my name is %s\n", name);
-}
-/**
  * print_name - prints a name
  * @name: string input
  * @f: pointer to function
@@ -16,5 +7,6 @@ void f(char *name)
 */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
