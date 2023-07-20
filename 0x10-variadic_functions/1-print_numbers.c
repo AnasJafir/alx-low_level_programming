ea07f9209d52343eb5069c57ca213377bb7a4b68
AnasJafir <dr.anas.jafir@gmail.com>
2023-07-20 06:42:02 -0700

    print all numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 	if (separator == NULL)
+	{
+		printf("\n");
 		return;
+	}
 	for (i = 0; i < n; i++)
 	{
 		nums = va_arg(args, int);
