8c3ebc3b069eb1787872f51b396589bcbaa00454
AnasJafir <dr.anas.jafir@gmail.com>
2023-07-27 18:45:31 -0700

    length of a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - claculate the length of list
+ * @h: pointer to the first node
+ * Return: the length of list
+*/
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while(h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
