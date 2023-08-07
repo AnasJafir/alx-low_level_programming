01c08174c7cf9a1ec622920353357d1963f97d89
AnasJafir <dr.anas.jafir@gmail.com>
2023-08-07 12:19:22 -0700

    copying content from file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,50 +1,33 @@
 #include "main.h"
 /**
- * main - Entry point
- * @argc: argument index
- * @argv: argument value
+ * main - entry point
+ * @argc: arguments index
+ * @argv: arguments value
  * Return: 1 on success, -1 on failure
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int file_from = 0, file_to = 0;
+	int f_from = 0, f_to = 0;
 	ssize_t bytes;
-	char buffer[1024];
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to"), exit(97);
-	}
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
-	while ((bytes = read(file_from, buffer, 1024)) > 0)
-	{
-		if (write(file_to, buffer, bytes) != bytes)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-		}
-	}
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+	f_from = open(argv[1], O_RDONLY);
+	if (f_from == -1)
+		dprintf(STDERR_FILENO, NO_READ, argv[1]), exit(98);
+	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (f_to == -1)
+		dprintf(STDERR_FILENO, NO_WRITE, argv[2]), exit(99);
+	while ((bytes = read(f_from, buffer, BUFFER_SIZE)) > 0)
+		if (write(f_to, buffer, bytes) != bytes)
+			dprintf(STDERR_FILENO, NO_WRITE, argv[2]), exit(99);
 	if (bytes == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	}
-	file_from = close(file_from);
-	file_to = close(file_to);
-	if (file_from)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-	}
-	if (file_to)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-	}
+		dprintf(STDERR_FILENO, NO_READ, argv[1]), exit(98);
+
+	f_from = close(f_from);
+	f_to = close(f_to);
+	if (f_from == -1 || f_to == -1)
+		dprintf(STDERR_FILENO, NO_CLOSE, f_from), exit(100);
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,12 @@
 #include <stdio.h>
 #include <fcntl.h>
 
+#define USAGE "Usage: cp file_from file_to\n"
+#define NO_READ "Error: Can't read from file %s\n"
+#define NO_WRITE "Error: Can't write to %s\n"
+#define NO_CLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define BUFFER_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
