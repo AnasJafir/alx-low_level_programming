c90c93c0a9d287cb29ee630015038701b76fedf4
AnasJafir <dr.anas.jafir@gmail.com>
2023-08-07 10:45:44 -0700

    appending text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -28,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	file = open(filename, O_WRONLY | O_TRUNC);
+	file = open(filename, O_WRONLY | O_APPEND);
 	if (text_content)
 	{
 		len = _strlen(text_content);
