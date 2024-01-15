fdf58737488ebe6a1af42e4776116ddab05baa1d
AnasJafir <dr.anas.jafir@gmail.com>
2024-01-15 09:59:33 -0800

    Search for a value in a sorted array of integers using linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ * linear_search - Searches for a value in a sorted arr wit linear search algo
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: first index where value is located or
+ *		-1 if value is not present in array
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
