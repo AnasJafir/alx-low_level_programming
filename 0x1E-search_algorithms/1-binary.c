432d65e54109a94a770269e30923f0b00defc18a
AnasJafir <dr.anas.jafir@gmail.com>
2024-03-12 08:58:18 -0700

    Correcting betty issues

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,34 +9,31 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t i, mid;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t i, mid;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
-
-    while (left <= right)
-    {
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            if (i == right)
-                printf("%d\n", array[i]);
-            else
-                printf("%d, ", array[i]);
-        }
-
-        mid = left + (right - left) / 2;
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-
-    return (-1);
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == right)
+				printf("%d\n", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+		mid = left + (right - left) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
 }
