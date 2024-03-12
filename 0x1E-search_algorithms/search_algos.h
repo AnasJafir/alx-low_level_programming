1f44b7cc5b9183114a778c0f5edf62ca71e97880
AnasJafir <dr.anas.jafir@gmail.com>
2024-03-12 15:29:44 +0000

    Linear Search Algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+
+
+
+#endif
